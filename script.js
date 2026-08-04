618ac8e9c167287d0c6bd9b2c07d509e3f6b46f8
anabellesor <anabellesor@gmail.com>
2026-08-04 00:20:36 -0400

    Complete Wix Samples page match
    * Complete Wix Samples gallery content
    
    * Match Wix Samples hero and carousel geometry
    
    * Match Wix Samples carousel controls

--- script.js
+++ script.js
@@ -34,8 +34,18 @@ if (carousel) {
     carousel.scrollBy({ left: carousel.clientWidth * direction, behavior: 'smooth' });
   };
 
-  document.querySelector('.carousel-prev').addEventListener('click', () => scrollSamples(-1));
-  document.querySelector('.carousel-next').addEventListener('click', () => scrollSamples(1));
+  const previousControl = document.querySelector('.carousel-prev');
+  const nextControl = document.querySelector('.carousel-next');
+
+  const updateCarouselControls = () => {
+    previousControl.hidden = carousel.scrollLeft < 5;
+    nextControl.hidden = carousel.scrollLeft + carousel.clientWidth >= carousel.scrollWidth - 5;
+  };
+
+  previousControl.addEventListener('click', () => scrollSamples(-1));
+  nextControl.addEventListener('click', () => scrollSamples(1));
+  carousel.addEventListener('scroll', updateCarouselControls, { passive: true });
+  updateCarouselControls();
 
   const lightbox = document.querySelector('.sample-lightbox');
   const lightboxImage = lightbox.querySelector('img');
